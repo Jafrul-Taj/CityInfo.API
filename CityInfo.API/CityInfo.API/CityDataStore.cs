384268b2d714d3e89964855ee06aafa6c41b2ee2
Jafrul Hasan Taj <jfhasan210@gmail.com>
2019-09-20 04:44:08 +0600

    Improving the architecture of the model class

---
+++ CityInfo.API/CityInfo.API/CityDataStore.cs
@@ -0,0 +1,44 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CityDataStore
+    {
+        public static CityDataStore Current { get; } = new CityDataStore();
+        public List<CityDto> Cities { get; set; }
+        public CityDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id=1,
+                    Name="Dhaka",
+                    Description="The City of Mosque."
+                },
+                new CityDto()
+                {
+                    Id=2,
+                    Name="Dinajpur",
+                    Description="The City of peace."
+                },
+                new CityDto()
+                {
+                    Id=3,
+                    Name="Chittagong",
+                    Description="the city by the sea "
+                }
+            };
+            
+            
+        }
+
+       
+
+       
+    }
+}
---
+++ CityInfo.API/CityInfo.API/Comtrollers/CitiesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Comtrollers
+{
+    [Route("api/[controller]")]
+    //[ApiController]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet()]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CityDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(
+                CityDataStore.Current.Cities.FirstOrDefault(c => c.Id==id));
+        }
+    }
+}
